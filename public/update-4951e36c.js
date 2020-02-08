beb4c9d285a46ba93b06e9e5be6882a126af8bae
BradMeyer-MSU <31320485+BradMeyer-MSU@users.noreply.github.com>
2020-02-07 22:38:08 -0600

    remove unneeded files from this branch

--- public/update-4951e36c.js
+++
@@ -1,8 +0,0 @@
-var span = document.querySelector('#hello-world');
-
-function update() {
-	span.textContent = 'Hello World';
-}
-
-export default update;
-//# sourceMappingURL=update-4951e36c.js.map
