866eb9ae85cedb19e8fd23e9e6adb89e25d41f17
BradMeyer-MSU <31320485+BradMeyer-MSU@users.noreply.github.com>
2020-02-18 00:38:19 -0600

    remove rollup and unneeded css and js files

--- public/main.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-	let dropdown1 = document.querySelector('.dropdown-content');
-	let instanceDropdown1 = M.Dropdown.init(dropdown1, dropdownOptions);
-});
-//# sourceMappingURL=main.js.map
--- src/main.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-document.addEventListener('DOMContentLoaded', function() {
-	let dropdown1 = document.querySelector('.dropdown-content');
-	let instanceDropdown1 = M.Dropdown.init(dropdown1, dropdownOptions);
-});
