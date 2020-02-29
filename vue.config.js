ca0ab26eb3c88379ad188c19ce499e1aa3e88c1e
BradMeyer-MSU <31320485+BradMeyer-MSU@users.noreply.github.com>
2020-02-29 02:39:32 -0600

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	pwa: {
+		name: 'Photo App',
+		themeColor: '#4DBA87',
+		msTileColor: '#000000',
+		appleMobileWebAppCapable: 'yes',
+		appleMobileWebAppStatusBarStyle: 'black',
+
+		// configure the workbox plugin
+		workboxPluginMode: 'GenerateSW',
+	}
+};
