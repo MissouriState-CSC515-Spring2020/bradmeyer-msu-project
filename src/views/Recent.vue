2283eac83d4a58e134b64c11d3315d8d56769108
BradMeyer-MSU <31320485+BradMeyer-MSU@users.noreply.github.com>
2020-03-04 01:06:07 -0600

    smaller image files

--- src/views/Recent.vue
+++ src/views/Recent.vue
@@ -7,40 +7,40 @@
     <div class="section">
       <div class="row">
         <div class="col s4">
-          <a href="photodetails"><img class="responsive-img hoverable" src="img/jqh_med.jpg"></a>
+          <a href="photodetails"><img class="responsive-img hoverable" src="img/jqh_small.jpg"></a>
         </div>
         <div class="col s4">
-          <a href="photodetails"><img class="responsive-img hoverable" src="img/basspro_med.jpg"></a>
+          <a href="photodetails"><img class="responsive-img hoverable" src="img/basspro_small.jpg"></a>
         </div>
         <div class="col s4">
-          <a href="photodetails"><img class="responsive-img hoverable" src="img/centralhs_med.jpg"></a>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col s4">
-          <a href="photodetails"><img class="responsive-img hoverable" src="img/drury_med.jpg"></a>
-        </div>
-        <div class="col s4">
-          <a href="photodetails"><img class="responsive-img hoverable" src="img/footbridge_med.jpg"></a>
-        </div>
-        <div class="col s4">
-          <a href="photodetails"><img class="responsive-img hoverable" src="img/greenway_med.jpg"></a>
+          <a href="photodetails"><img class="responsive-img hoverable" src="img/centralhs_small.jpg"></a>
         </div>
       </div>
       <div class="row">
         <div class="col s4">
-          <a href="photodetails"><img class="responsive-img hoverable" src="img/mercy_med.jpg"></a>
+          <a href="photodetails"><img class="responsive-img hoverable" src="img/drury_small.jpg"></a>
         </div>
         <div class="col s4">
-          <a href="photodetails"><img class="responsive-img hoverable" src="img/mexicanvilla_med.jpg"></a>
+          <a href="photodetails"><img class="responsive-img hoverable" src="img/footbridge_small.jpg"></a>
         </div>
         <div class="col s4">
-          <a href="photodetails"><img class="responsive-img hoverable" src="img/springfield_med.jpg"></a>
+          <a href="photodetails"><img class="responsive-img hoverable" src="img/greenway_small.jpg"></a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col s4">
+          <a href="photodetails"><img class="responsive-img hoverable" src="img/mercy_small.jpg"></a>
+        </div>
+        <div class="col s4">
+          <a href="photodetails"><img class="responsive-img hoverable" src="img/mexicanvilla_small.jpg"></a>
+        </div>
+        <div class="col s4">
+          <a href="photodetails"><img class="responsive-img hoverable" src="img/springfield_small.jpg"></a>
         </div>
       </div>
       <div class="row">
         <div class="col s4 offset-s4">
-          <a href="photodetails"><img class="responsive-img hoverable" src="img/tumbler_med.jpg"></a>
+          <a href="photodetails"><img class="responsive-img hoverable" src="img/tumbler_small.jpg"></a>
         </div>
       </div>
     </div>
