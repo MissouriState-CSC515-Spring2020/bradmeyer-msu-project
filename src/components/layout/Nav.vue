b7c8ddf6ed7e32730012b7e68037533242f8d9e3
BradMeyer-MSU <31320485+BradMeyer-MSU@users.noreply.github.com>
2020-03-04 01:06:27 -0600

    update navbar with logo image

--- src/components/layout/Nav.vue
+++ src/components/layout/Nav.vue
@@ -2,18 +2,24 @@
   <div>
     <nav class="teal darken-3">
       <div class="nav-wrapper container">
-        <a id="logo-container" href="/" class="brand-logo">Photo App</a>
-        <ul class="right hide-on-med-and-down">
-          <li><router-link to="/">Recent</router-link></li>
-          <!-- Dropdown Trigger -->
-          <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Categories<i class="material-icons right">arrow_drop_down</i></a></li>
-        </ul>
-        <a href="#" data-target="mobile-menu" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-        <ul id="mobile-menu" class="sidenav">
-          <li><router-link to="/">Recent</router-link></li>
-          <!-- Dropdown Trigger -->
-          <li><a class="dropdown-trigger" href="#!" data-target="dropdown2">Categories<i class="material-icons right">arrow_drop_down</i></a></li>
-        </ul>
+          <span id="logo-container">
+            <a href="/"><img class="icon hide-on-med-and-down" src="/img/icons/icons8-photo-gallery-96.png" width="65"></a>
+            <a href="/" class="brand-logo">Photo App</a>
+          </span>
+
+          <ul class="right hide-on-med-and-down">
+            <li><router-link to="/">Recent</router-link></li>
+            <!-- Dropdown Trigger -->
+            <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Categories<i class="material-icons right">arrow_drop_down</i></a></li>
+          </ul>
+
+          <a href="#" data-target="mobile-menu" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+
+          <ul id="mobile-menu" class="sidenav">
+            <li><router-link to="/">Recent</router-link></li>
+            <!-- Dropdown Trigger -->
+            <li><a class="dropdown-trigger" href="#!" data-target="dropdown2">Categories<i class="material-icons right">arrow_drop_down</i></a></li>
+          </ul>
       </div>
     </nav>
 
