375ffa889d90c700163b8edb3e6ad6005900e1bd
BradMeyer-MSU <31320485+BradMeyer-MSU@users.noreply.github.com>
2020-04-21 01:59:03 -0500

    use date-fns to display friendlier timestamps

--- src/components/widgets/VideoCard.vue
+++ src/components/widgets/VideoCard.vue
@@ -4,11 +4,12 @@
       <img class="responsive-img hoverable" v-bind:src=info.snippet.thumbnails.high.url>
       {{ info.snippet.title }}
     </router-link>
-    <div>{{ info.snippet.publishedAt }}</div>
+    <div>{{ publishedAt.time }} {{ publishedAt.date }}</div>
   </div>
 </template>
 
 <script>
+import { parseISO, format } from 'date-fns'
 export default {
   name: 'VideoCard',
   props: ["info"],
@@ -20,6 +21,13 @@ export default {
         return this.info.snippet.resourceId.videoId
       } else {
         return ""
+      }
+    },
+    publishedAt: function() {
+      let d = parseISO(this.info.snippet.publishedAt);
+      return {
+        date: format(d, 'M/d/y'),
+        time: format(d, 'HH:mm')
       }
     }
   }
